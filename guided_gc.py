43f1fba5c43fcf5530f619f4d197923920c28210
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2022-09-11 15:37:42 +0900

    Update guided_gc.py

--- guided_gc.py
+++ guided_gc.py
@@ -7,7 +7,7 @@ from omegaconf import DictConfig
 from utils import load_model_pth, ImageLoader, DataTransform
 
 
-@hydra.main(version_base=None, config_path='config', config_name='config')
+@hydra.main(version_base=None, config_path='config', config_name='guided_gc')
 def main(cfg: DictConfig):
 
     print(f'target: {cfg.target}')
